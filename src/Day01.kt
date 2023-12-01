310a8c42d9fedf24b22d9b076be36c96295298d7
David <david.kozak@oracle.com>
2023-12-01 17:49:07 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
+val digits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
 fun main() {
+    fun extractNum(str: String, part2: Boolean = false): Int {
+        var fst = -1
+        var last = -1
+        for (i in str.indices) {
+            val c = str[i]
+            if (c in '0'..'9') {
+                if (fst == -1) fst = c - '0'
+                last = c - '0'
+            } else if (part2) {
+                l@ for ((x, digit) in digits.withIndex()) {
+                    for (j in digit.indices) {
+                        if (i + j >= str.length || str[i + j] != digit[j]) continue@l
+                    }
+                    if (fst == -1) fst = x + 1
+                    last = x + 1
+                }
+            }
+        }
+        return fst * 10 + last
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { extractNum(it) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { extractNum(it, true) }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
